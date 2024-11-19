3becef4b0848b0495e10f2c13c67e9f1b0aad351
Erick Galdámez <91312014+ErickAlas07@users.noreply.github.com>
2024-11-18 19:20:56 -0600

    Add logic to find category by id

--- src/main/java/com/ealas/inventory/controller/CategoryRestController.java
+++ src/main/java/com/ealas/inventory/controller/CategoryRestController.java
@@ -5,19 +5,26 @@ import com.ealas.inventory.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/category")
+@RequestMapping("/api/v1")
 public class CategoryRestController {
 
     @Autowired
     private ICategoryService categoryService;
 
-    @GetMapping
+    @GetMapping("/categories")
     public ResponseEntity<CategoryResponseRest> viewAll() {
         ResponseEntity<CategoryResponseRest> response = categoryService.search();
+        return response;
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> viewById(@PathVariable Long id) {
+        ResponseEntity<CategoryResponseRest> response = categoryService.findById(id);
         return response;
     }
 }
--- src/main/java/com/ealas/inventory/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ealas/inventory/service/impl/CategoryServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService {
@@ -21,6 +23,7 @@ public class CategoryServiceImpl implements ICategoryService {
     @Override
     @Transactional(readOnly = true)
     public ResponseEntity<CategoryResponseRest> search() {
+
         CategoryResponseRest response = new CategoryResponseRest();
         try {
             List<Category> categories = (List<Category>) categoryDao.findAll();
@@ -29,6 +32,34 @@ public class CategoryServiceImpl implements ICategoryService {
             response.setMetadata("success", "200", "2024-11-11");
         } catch (Exception e){
             response.setMetadata("error", "500", "2024-11-11");
+            e.getStackTrace();
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<CategoryResponseRest> findById(Long id) {
+
+        CategoryResponseRest response = new CategoryResponseRest();
+        List<Category> list = new ArrayList<>();
+
+        try {
+
+            Optional<Category> category = categoryDao.findById(id);
+
+            if(category.isPresent()){
+                list.add(category.get());
+                response.getCategoryResponse().setCategories(list);
+                response.setMetadata("success", "200", "2024-11-11");
+            } else {
+                response.setMetadata("error", "404", "No se encontró la categoría");
+                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+            }
+
+        } catch (Exception e){
+            response.setMetadata("error", "-1", "Error al consultar por ID");
             e.getStackTrace();
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
